0c8b14c1cc486187af7354b5411af15efa6875f3
thestarrynight17 <thestarrynight17@users.noreply.github.com>
2024-05-23 19:07:15 +0400

    Task 2 added

--- ArrayPractice/Program.cs
+++ ArrayPractice/Program.cs
@@ -15,6 +15,40 @@ namespace ArrayPractice
                 Console.WriteLine(numbers[i]);
             }
             #endregion
+
+
+            #region Task2
+            int[] arr1 = { 1, 2, 3 };
+            int[] arr2 = { 1, 2, 4, 5, 3 };
+            bool allIncluded = true;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                bool isFound = false;
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if (arr1[i] == arr2[j])
+                    {
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (!isFound)
+                {
+                    allIncluded = false;
+                    break;
+                }
+            }
+
+            if (allIncluded)
+            {
+                Console.WriteLine("All numbers from the first array are included in the second array.");
+            }
+            else
+            {
+                Console.WriteLine("Not all numbers from the first array are included in the second array.");
+            }
+            #endregion
         }
     }
 }
