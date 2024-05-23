db473e486f3182456914e52aa3b8b2558e82644d
thestarrynight17 <thestarrynight17@users.noreply.github.com>
2024-05-23 19:06:37 +0400

    Task 1 added

--- ArrayPractice/Program.cs
+++ ArrayPractice/Program.cs
@@ -6,7 +6,15 @@ namespace ArrayPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Task1
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int lastIdx = numbers.Length - 1;
+
+            for (int i = lastIdx; i >= 0; i--)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            #endregion
         }
     }
 }
